c536bdfbc5f723c013f0e16a6fd390673da583c8
ambreen81 <ambreen.asad2024@gmail.com>
2026-04-23 17:24:00 +0500

    Update app.py

--- app.py
+++ app.py
@@ -43,7 +43,8 @@ def generate_text(user_input):
         return "⚠️ Gemini API key missing."
 
     try:
-       model = genai.GenerativeModel("gemini-1.5-flash")
+        model = genai.GenerativeModel("gemini-1.5-flash")
+
         prompt = f"""
 You are an Amazon product research expert.
 
